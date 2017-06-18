23ad24410ee4cf4e3d750113d439acb6c7b7bff9
Jieyang Chen <cjy7117@gmail.com>
2017-06-18 16:06:54 -0600

    test

--- dgemm1.cu
+++ dgemm1.cu
@@ -328,7 +328,7 @@ void test_kernel4_1(int m, int n, int k,
 				    double * dA, int lda, 
 				    double * dB, int ldb, 
 				    double * dC, int ldc){    
-    int T = 128;
+    int T = 16;
     int tt = 4;
     int blocksPerGrid = m / T;
     int threadsPerBlock = T;
