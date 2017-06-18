7b836852eccbf5aaca86f427befa4d41747a8eac
Jieyang Chen <cjy7117@gmail.com>
2017-06-17 21:26:24 -0600

    test

--- dgemm1.cu
+++ dgemm1.cu
@@ -73,7 +73,7 @@ void test(int m, int k);
 
 int main(){
 	for (int i = 128; i <= 32768; i *= 2){
-		//i = 512;
+		i = 20480;
 		cout << "Test on: A (" << i << " x " << i << ") by B (" << i << " x " << 1 << ")" << endl;
 		test(i, i);
 	}
