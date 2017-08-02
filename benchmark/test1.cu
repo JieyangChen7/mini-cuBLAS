a62a50617ab0c55ce7160f2920d7f80a62e8747c
Jieyang Chen <cjy7117@gmail.com>
2017-08-02 15:17:12 -0600

    test

--- benchmark/test1.cu
+++ benchmark/test1.cu
@@ -9,10 +9,14 @@ __global__ void array_generator(int n, double * A) {
 
 
 int main(){
-  int n = 1024;
-  int B = 128;
+  int n = 128;
+  int B = 16;
   double * A = new double[n + B];
   double * dA;
   cudaMalloc(&dA, (n + B) * sizeof(double));
-
+  array_generator<<<n/B, B>>>(n, dA);
+  cudaMemcpy(A, dA, (n + B) * sizeof(double), cudaMemcpyDeviceToHost);
+  for (int i = 0; i < n + B; i++) {
+  	cout << A[i] << " ";
+  }
 }
