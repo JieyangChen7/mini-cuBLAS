20666f35797039576800edb294b8939143e8f0bf
Jieyang Chen <cjy7117@gmail.com>
2017-08-02 15:29:07 -0600

    test

--- benchmark/test1.cu
+++ benchmark/test1.cu
@@ -2,20 +2,20 @@
 #include <stdio.h>
 using namespace std;
 
-__global__ void array_generator(int n, double * A) {
+__global__ void array_generator(int n, double ** A) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  A[idx] = double(A + idx + blockDim.x);
+  A[idx] = A + idx + blockDim.x;
 }
 
 
 int main(){
   int n = 128;
   int B = 16;
-  double * A = new double[n + B];
-  double * dA;
-  cudaMalloc(&dA, (n + B) * sizeof(double));
+  double ** A = new double[n + B];
+  double ** dA;
+  cudaMalloc(&dA, (n + B) * sizeof(double*));
   array_generator<<<n/B, B>>>(n, dA);
-  cudaMemcpy(A, dA, (n + B) * sizeof(double), cudaMemcpyDeviceToHost);
+  cudaMemcpy(A, dA, (n + B) * sizeof(double*), cudaMemcpyDeviceToHost);
   for (int i = 0; i < n + B; i++) {
   	cout << A[i] << " ";
   }
