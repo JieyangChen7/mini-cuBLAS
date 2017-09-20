d8286111be1f6313e9bb96cf37c96df627f67d7a
Jieyang Chen <cjy7117@gmail.com>
2017-09-19 21:13:37 -0600

    test

--- benchmark/test1.cu
+++ benchmark/test1.cu
@@ -36,9 +36,9 @@ __global__ void tid_time(int iteration, unsigned long long int * T) {
   volatile clock_t end = 0;
   unsigned long long sum_time = 0;
   volatile register int idx2 = 0;
-  volatile register int a = blockIdx.x;
-  volatile register int b = blockDim.x;
-  volatile register int c = threadIdx.x;
+   register int a = blockIdx.x;
+   register int b = blockDim.x;
+   register int c = threadIdx.x;
   for (int i = 0; i < iteration; i++) {
     start = clock();
     idx2 = a * b + c;
