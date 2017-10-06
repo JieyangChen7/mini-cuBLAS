197b8323912085f47847ce98e30990367d0ef95a
Jieyang Chen <cjy7117@gmail.com>
2017-10-06 15:32:45 -0600

    Test

--- dgemm2.cu
+++ dgemm2.cu
@@ -98,8 +98,8 @@ dgemm_kernel_shared(int m, int n, int k, int T, double * A, int lda, double * B,
   register double a = 0;
 
   for (int j = 0; j < k; j += T){
-    //cache[threadIdx.x * 2] = *(B + threadIdx.x);
-    //cache[threadIdx.x * 2 + 1] = *(B + threadIdx.x + ldb);
+    cache[threadIdx.x * 2] = *(B + threadIdx.x);
+    cache[threadIdx.x * 2 + 1] = *(B + threadIdx.x + ldb);
     __syncthreads();
     B += T;
     for (int i = 0; i < T; i++) {
