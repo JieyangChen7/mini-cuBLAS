28aada77fe78a7b0f4d7f221a7959174f49b58cc
Jieyang Chen <cjy7117@gmail.com>
2017-10-06 12:28:22 -0600

    Test

--- benchmark/gl_access.cu
+++ benchmark/gl_access.cu
@@ -999,7 +999,7 @@ __global__ void global_memory_256(double * A, int iteration, int access_per_iter
 
 void test_2048(int block_size){
   int iteration = 1000;
-  int access_per_iter = 1;
+  int access_per_iter = 7;
   int SM = 15;
   int block_per_sm = 2048/block_size;
   int total_block = SM * block_per_sm;
