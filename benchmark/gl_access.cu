9ff87d55996f96aab4e65d42eb3d779a3227f5d2
Jieyang Chen <cjy7117@gmail.com>
2017-10-11 13:58:07 -0600

    Test

--- benchmark/gl_access.cu
+++ benchmark/gl_access.cu
@@ -37,10 +37,10 @@ __global__ void global_memory_2048(double * A, int iteration, int access_per_ite
    register double * a_next2 = A + LL;
    register double * a_next3 = A + LL * 2;
    register double * a_next4 = A + LL * 3;
-   // register double * a_next5 = A + LL * 4;
-   // register double * a_next6 = A + LL * 5;
-   // register double * a_next7 = A + LL * 6;
-  // register double * a_next8 = A + LL * 7;
+   register double * a_next5 = A + LL * 4;
+   register double * a_next6 = A + LL * 5;
+   register double * a_next7 = A + LL * 6;
+  //register double * a_next8 = A + LL * 7;
 
    register double temp = 0;
   
@@ -55,6 +55,22 @@ __global__ void global_memory_2048(double * A, int iteration, int access_per_ite
     a_next3 = (double *)(unsigned long long int) *a_next3;
     a_next4 = (double *)(unsigned long long int) *a_next4;
     
+
+    a_next5 = (double *)(unsigned long long int) *a_next5;
+    a_next6 = (double *)(unsigned long long int) *a_next6;
+    a_next7 = (double *)(unsigned long long int) *a_next7;
+  // a_next8 = (double *)(unsigned long long int) *a_next8;
+
+     temp += temp * iteration;
+    temp += temp *iteration;
+    temp += temp *iteration;
+    temp += temp *iteration;
+
+    temp += temp * iteration;
+    temp += temp *iteration;
+    temp += temp *iteration;
+    temp += temp *iteration;
+
     temp += temp * iteration;
     temp += temp *iteration;
     temp += temp *iteration;
@@ -65,19 +81,7 @@ __global__ void global_memory_2048(double * A, int iteration, int access_per_ite
     temp += temp *iteration;
     temp += temp *iteration;
 
-    // temp += temp * iteration;
-    // temp += temp *iteration;
-    // temp += temp *iteration;
-    // temp += temp *iteration;
 
-    // temp += temp * iteration;
-    // temp += temp *iteration;
-    // temp += temp *iteration;
-    // temp += temp *iteration;
-    // a_next5 = (double *)(unsigned long long int) *a_next5;
-    // a_next6 = (double *)(unsigned long long int) *a_next6;
-    // a_next7 = (double *)(unsigned long long int) *a_next7;
-  //  a_next8 = (double *)(unsigned long long int) *a_next8;
     //__syncthreads();
     
     //end = clock(); 
@@ -88,10 +92,10 @@ __global__ void global_memory_2048(double * A, int iteration, int access_per_ite
   *A +=  (unsigned long long int)a_next3;
   *A +=  (unsigned long long int)a_next4;
   *A += temp;
-  // *A +=  (unsigned long long int)a_next5;
-  // *A +=  (unsigned long long int)a_next6;
-  // *A +=  (unsigned long long int)a_next7;
-  //*A +=  (unsigned long long int)a_next8;
+  *A +=  (unsigned long long int)a_next5;
+  *A +=  (unsigned long long int)a_next6;
+  *A +=  (unsigned long long int)a_next7;
+ // *A +=  (unsigned long long int)a_next8;
 
 }
 
@@ -977,7 +981,7 @@ __global__ void global_memory_256(double * A, int iteration, int access_per_iter
 
 void test_2048(int block_size){
   int iteration = 1000;
-  int access_per_iter = 4;
+  int access_per_iter = 7;
   //int SM = 24;
   int block_per_sm = 2048/block_size;
   int total_block = SM * block_per_sm;
