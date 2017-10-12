57d49ae73b6235c420bd7fe7a3c8ef16e911c3fb
Jieyang Chen <cjy7117@gmail.com>
2017-10-12 13:43:20 -0600

    Test

--- benchmark/complile_test1.sh
+++ benchmark/complile_test1.sh
@@ -1,2 +1,2 @@
-#nvcc -O0 -Xcicc -O3 -Xptxas -O3 --ptxas-options=-v -arch=sm_35 gl_access.cu -lcublas
-nvcc -O0 -Xcicc -O3 -Xptxas -O3 --ptxas-options=-v -arch=sm_35 fmad.cu -lcublas
+nvcc -O0 -Xcicc -O3 -Xptxas -O3 --ptxas-options=-v -arch=sm_35 gl_access.cu -lcublas -o gl_access
+nvcc -O0 -Xcicc -O3 -Xptxas -O3 --ptxas-options=-v -arch=sm_35 fmad.cu -lcublas -o fmad
