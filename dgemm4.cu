1702105e767c639c392d34e0f8bf918d0beed8b8
Jieyang Chen <cjy7117@gmail.com>
2020-11-10 13:30:35 -0500

    Update dgemm4.cu

--- dgemm4.cu
+++ dgemm4.cu
@@ -1179,7 +1179,7 @@ float test_cublas_mm(int m, int n, int k,
 void test(int m, int k, int c);
 
 int main(int argc, char *argv[]){
-  for (int i = 10240; i <= 30720; i += 1024){
+  for (int i = 10240; i <= 40960; i += 1024){
   //  int i = 6144;
     // cout << "Test on: A (" << i << " x " << i << ") by B (" << i << " x " << 4 << ")" << endl;
     test(i, i, atoi(argv[1]));
